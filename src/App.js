3c104b99446c900141eb5ac545d356049b4b3349
michelleRahman21 <michgomez1807@hotmail.com>
2020-04-24 13:29:42 -0400

    Work on box for FaceRecognition

--- src/App.js
+++ src/App.js
@@ -37,7 +37,12 @@ class App extends Component {
 }
 
 calculateFaceLocation = (data) => {
-  
+  const clarifaiFace =  data.outputs[0].data.regions[0].region_info.bounding_box;
+  const image = document.getElementById('inputimage');
+  const width = Number(image.width);
+  const height = Number(image.height);
+
+  console.log(width, height);
 }
 
 
@@ -47,7 +52,7 @@ onInputChange = (event) => {
 
 onButtonSubmit = () => {
   this.setState=({imageUrl: this.state.input});
-  clarifaiApp.models
+  app.models
   .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
   .then((response) => this.displayFaceBox(this.calculateFaceLocation(response)).catch((err) => console.log(err)));
 };
