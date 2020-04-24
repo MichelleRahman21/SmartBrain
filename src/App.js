6b467a3b6dd61302ef485819fbc6b2f0c6044bc9
michelleRahman21 <michgomez1807@hotmail.com>
2020-04-24 13:11:21 -0400

    Added pieces from the API
    Bug Fix

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Particles from 'react-particles-js';
-import Clarifai from 'clarifai';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Navigation from './components/Navigation/Navigation';
+import Clarifai from 'clarifai';
+
 import Logo from './components/Logo/Logo';
-import Rank from './components/Rank/Rank';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+
 import './App.css';
 
 const app = new Clarifai.App({
@@ -29,26 +31,26 @@ class App extends Component {
     super();
     this.state = {
       input: '',
-      imageUrl: ''
+      imageUrl: '',
+      box: {},
     }
 }
 
-  onInputChange = (event) => {
+calculateFaceLocation = (data) => {
+  
+}
+
+
+onInputChange = (event) => {
   this.setState({input: event.target.value});
 }
 
 onButtonSubmit = () => {
   this.setState=({imageUrl: this.state.input});
-  app.models.predict(Clarifai.FACE_DETECT, this.state.input)
-  .then(
-    function(response) {
-      console.log(response);
-    },
-    function(err) {
-      // there was an error
-    }
-  );
-}
+  clarifaiApp.models
+  .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+  .then((response) => this.displayFaceBox(this.calculateFaceLocation(response)).catch((err) => console.log(err)));
+};
 
   render() {
   return (
