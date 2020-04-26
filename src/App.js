3e0eec0d71b3d63bc6c2a116582c14706ef263d6
michelleRahman21 <michgomez1807@hotmail.com>
2020-04-26 16:43:14 -0400

    Finish FaceRecognition box
    Bug Fix / API update

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ class App extends Component {
     this.state = {
       input: '',
       imageUrl: '',
-      box: {},
+      box: {}
     }
 }
 
@@ -52,7 +52,6 @@ calculateFaceLocation = (data) => {
 
 displayFaceBox = (box) => {
   this.setState({box: box});
-  console.log(box)
 }
 
 
@@ -61,21 +60,40 @@ onInputChange = (event) => {
 }
 
 onButtonSubmit = () => {
-  this.setState=({imageUrl: this.state.input});
-  app.models
-  .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
-  .then((response) => this.displayFaceBox(this.calculateFaceLocation(response)).catch((err) => console.log(err)));
-};
+   this.setState({imageUrl: this.state.input});
+   app.models
+     .predict(
+       Clarifai.FACE_DETECT_MODEL,
+       this.state.input)
+     .then(response => {
+       if (response) {
+         fetch('http://localhost:3000/image', {
+           method: 'put',
+           headers: {'Content-Type': 'application/json'},
+           body: JSON.stringify({
+             id: this.state.user.id
+           })
+         })
+           .then(response => response.json())
+           .then(count => {
+             this.setState(Object.assign(this.state.user, { entries: count}))
+           })
+
+       }
+       this.displayFaceBox(this.calculateFaceLocation(response))
+     })
+     .catch(err => console.log(err));
+ }
 
   render() {
   return (
     <div className="App">
-      <Particles className='particles' params={particlesOptions}/>
+      <Particles className='particles' params={particlesOptions} />
       <Navigation />
       <Logo />
       <Rank />
-      <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
-      <FaceRecognition imageUrl={this.state.imageUrl} />
+      <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
+      <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
     </div>
   );
 }
