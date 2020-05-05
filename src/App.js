648ec9d604d02f9c3dbd85772250fc6e55263a2e
michelleRahman21 <michgomez1807@hotmail.com>
2020-05-05 13:33:59 -0400

    Destructuring and refactoring
    Debugging

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ class App extends Component {
       imageUrl: '',
       box: {},
       route: 'signin',
-      isSignedIn: 'false'
+      isSignedIn: false
     }
 }
 
@@ -99,13 +99,14 @@ onRouteChange = (route) => {
 }
 
 render() {
+const { isSignedIn, imageURL, route, box } = this.state;
   return (
         <div className="App">
           <Particles className='particles'
           params={particlesOptions}
           />
-          <Navigation onRouteChange={this.onRouteChange} />
-            { this.state.route === 'home '
+          <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} />
+            { route === 'home '
             ? <div>
             <Logo />
             <Rank />
@@ -113,10 +114,10 @@ render() {
               onInputChange={this.onInputChange}
               onButtonSubmit={this.onButtonSubmit}
             />
-            <FaceRecognition   box={this.state.box} imageURL={this.state.imageURL} />
+            <FaceRecognition   box={box} imageURL={imageURL} />
             </div>
             :(
-              this.state.route === 'signin'
+              route === 'signin'
               ? <Signin onRouteChange={this.onRouteChange} />
               : <Register onRouteChange={this.onRouteChange} />
             )
