98a24b48142804092c29671e74c3ae41fe55899f
michelleRahman21 <michgomez1807@hotmail.com>
2020-05-04 20:28:26 -0400

    Added state for sign in Created Sign out in nav

--- src/App.js
+++ src/App.js
@@ -35,7 +35,8 @@ class App extends Component {
       input: '',
       imageUrl: '',
       box: {},
-      route: 'signin'
+      route: 'signin',
+      isSignedIn: 'false'
     }
 }
 
@@ -89,6 +90,11 @@ onButtonSubmit = () => {
  }
 
 onRouteChange = (route) => {
+  if (route === 'signout') {
+    this.setState({isSignedIn: false})
+  }else if(route === 'home') {
+    this.setState({isSignedIn: true})
+  }
   this.setState({route: route});
 }
 
